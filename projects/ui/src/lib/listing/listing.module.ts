8f67a7897c31805a0ea778e75283cdbb140904df
mk <v.a.stekolschikov@gmail.com>
2020-11-05 17:05:07 +0200

    add mixin

--- projects/ui/src/lib/listing/listing.module.ts
+++ projects/ui/src/lib/listing/listing.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common'
 import { ListingComponent } from './listing.component'
 import { RouterModule } from '@angular/router'
 import { PipesModule } from '@libs/pipes/pipes.module'
+import { TranslocoModule } from '@ngneat/transloco'
 
 @NgModule({
   declarations: [ListingComponent],
   imports: [
     CommonModule,
     RouterModule,
+    TranslocoModule,
     PipesModule
   ],
   exports: [ListingComponent]
