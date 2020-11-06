0be89856e98b8972f45c0e466c71d29133bd1ade
Albert Iblyaminov <albert@iblyaminov.io>
2020-11-06 18:31:24 +0300

    Added deploy with github actions

---
+++ heroku.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Waves Grants',
+      script: 'serve',
+      // instances: process.env.INSTANCES || "1",
+      // max_memory_restart: process.env.MEMORY || "256M",
+      cwd: './dist/dapp',
+      env: {
+        NODE_ENV: 'production',
+        PM2_SERVE_PATH: '.',
+        PM2_SERVE_PORT: process.env.PORT,
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: '/index.html'
+      }
+    }
+  ]
+}
