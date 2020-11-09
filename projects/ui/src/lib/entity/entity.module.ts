f28bbadd6c5bec322064a8381e2f77a9eb2bb27f
mk <v.a.stekolschikov@gmail.com>
2020-11-09 11:31:12 +0200

    add card header style

--- projects/ui/src/lib/entity/entity.module.ts
+++ projects/ui/src/lib/entity/entity.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { EntityComponent } from './entity.component'
+import { TranslocoModule } from '@ngneat/transloco'
 
 @NgModule({
   declarations: [EntityComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslocoModule
   ],
   exports: [EntityComponent]
 })
