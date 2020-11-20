2ae96bdf30082784780cd1cf780f1c65d5ff8a09
mk <v.a.stekolschikov@gmail.com>
2020-11-20 17:35:40 +0200

    clear controls

--- projects/dapp/src/app/pages/team-page/team-page.component.spec.ts
+++ projects/dapp/src/app/pages/team-page/team-page.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { TeamPageComponent } from './team.component';
+import { TeamPageComponent } from './team-page.component'
 
 describe('TeamComponent', () => {
-  let component: TeamPageComponent;
-  let fixture: ComponentFixture<TeamPageComponent>;
+  let component: TeamPageComponent
+  let fixture: ComponentFixture<TeamPageComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TeamPageComponent ]
+      declarations: [TeamPageComponent]
     })
-    .compileComponents();
-  });
+      .compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TeamPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(TeamPageComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
