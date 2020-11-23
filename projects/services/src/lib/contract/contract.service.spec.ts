9788049570d50b2b3eccf5a2042d65c7d1140af5
mk <v.a.stekolschikov@gmail.com>
2020-11-23 14:59:22 +0200

    fix tests

--- projects/services/src/lib/contract/contract.service.spec.ts
+++ projects/services/src/lib/contract/contract.service.spec.ts
@@ -2,13 +2,14 @@ import { TestBed } from '@angular/core/testing'
 import { ContractService } from './contract.service'
 import { provideApi } from '@constants'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('ContractService', () => {
   let service: ContractService
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule],
       providers: [
         provideApi()
       ]
