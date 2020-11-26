33c335eb43f098cbc9e0bed85facec37c3a3959c
mk <v.a.stekolschikov@gmail.com>
2020-11-26 14:54:57 +0200

    fix reward input

--- projects/dapp/src/app/pages/create-grant-page/create-grant-page-routing.module.ts
+++ projects/dapp/src/app/pages/create-grant-page/create-grant-page-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     path: '',
     component: CreateGrantPageComponent,
     pathMatch: 'full',
-    // canActivate: [CreateGrantPageGuard],
+    canActivate: [CreateGrantPageGuard],
     data: {}
   }
 ]
