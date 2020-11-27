6f16caa8afb8dfaa893f97e8a71d261742eeff3b
mk <v.a.stekolschikov@gmail.com>
2020-11-28 00:33:22 +0200

    create-grant page

--- projects/libs/src/lib/directives/reward.directive.ts
+++ projects/libs/src/lib/directives/reward.directive.ts
@@ -5,23 +5,15 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class RewardDirective {
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef) {
+  }
 
-  @HostListener('input', ['$event.target.value']) input() {
-    if(this.el.nativeElement.value) {
-      this.el.nativeElement.value = this.format(this.el.nativeElement.value)
-    }
+  @HostListener('blur') blur() {
+    this.el.nativeElement.value = this.format(this.el.nativeElement.value)
   }
 
   format(str: string) {
-    str = parseInt(str.replace(/\D+/g,"")).toString()
-    if(str.length < 3) {
-      str += '00'
-    }
-    if(str.length > 2) {
-      str = str.substr(0, str.length - 2) + '.' + str.substr(str.length - 2, str.length)
-    }
-    return str
+    return parseFloat(str).toFixed(2).toString()
   }
 
 }
