a55dbb9e814cbe9d33826770654fbd8ecbfedca6
mk <v.a.stekolschikov@gmail.com>
2020-11-30 15:36:15 +0200

    add sortByTeam

--- projects/libs/src/lib/pipes/pipes.module.ts
+++ projects/libs/src/lib/pipes/pipes.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RelativeRoutePipe, RoutePipe, RoutesPipe } from './route-path.pipe'
-import {SortByTeamPipe} from "@libs/pipes/sort-by-team.pipe";
-
+import { SortByTeamPipe } from '@libs/pipes/sort-by-team.pipe'
 
 @NgModule({
   declarations: [
--- projects/libs/src/lib/pipes/sort-by-team.pipe.ts
+++ projects/libs/src/lib/pipes/sort-by-team.pipe.ts
@@ -8,7 +8,7 @@ import { ContractGrantAppModel } from '@services/contract/contract.model'
 
 @Pipe({ name: 'sortByTeam' })
 export class SortByTeamPipe implements PipeTransform {
-  transform (value: ContractGrantAppModel[], order = '', column: string = ''): any[] {
+  transform (value: ContractGrantAppModel[], order = '', column: string = ''): ContractGrantAppModel[] {
     value = _.orderBy(value, ['score', 'value'], ['asc'])
     value = _.orderBy(value, ['process', 'value'], ['asc'])
     return value
