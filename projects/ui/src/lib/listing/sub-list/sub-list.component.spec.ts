2edda906aa787b83160e9ed7b088f0618e493c68
mk <v.a.stekolschikov@gmail.com>
2020-12-03 15:35:17 +0200

    rn ui-sub-list

--- projects/ui/src/lib/listing/sub-list/sub-list.component.spec.ts
+++ projects/ui/src/lib/listing/sub-list/sub-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { SubListComponent } from './sub-list.component'
+import { provideApi, provideAppConstants } from '@constants'
 
 describe('CardComponent', () => {
   let component: SubListComponent
@@ -8,7 +9,11 @@ describe('CardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SubListComponent]
+      declarations: [SubListComponent],
+      providers: [
+        provideAppConstants(),
+        provideApi()
+      ]
     })
       .compileComponents()
   })
