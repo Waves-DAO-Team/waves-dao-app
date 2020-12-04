59a0e7c3c5a83702aa817caeb496d066282bf59f
mk <v.a.stekolschikov@gmail.com>
2020-12-04 17:21:35 +0200

    add translate

--- projects/dapp/src/app/pages/about-page/about-page.module.ts
+++ projects/dapp/src/app/pages/about-page/about-page.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common'
 
 import { AboutPageRoutingModule } from './about-page-routing.module'
 import { AboutPageComponent } from './about-page.component'
+import { TranslocoModule } from '@ngneat/transloco'
 
 @NgModule({
   declarations: [AboutPageComponent],
   imports: [
     CommonModule,
-    AboutPageRoutingModule
+    AboutPageRoutingModule,
+    TranslocoModule
   ]
 })
 export class AboutPageModule { }
