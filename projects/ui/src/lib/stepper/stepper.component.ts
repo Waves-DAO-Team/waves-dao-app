806fa835a802e6d6c124e9a284f16d5210c96c18
mk <v.a.stekolschikov@gmail.com>
2020-12-17 10:27:36 +0200

    fix stepper

--- projects/ui/src/lib/stepper/stepper.component.ts
+++ projects/ui/src/lib/stepper/stepper.component.ts
@@ -1,8 +1,10 @@
-import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MatStepper} from "@angular/material/stepper";
 import {GrantStatusEnum} from "@services/static/static.model";
 import {StepperService} from "@services/stepper/stepper.service";
+import {combineLatest, Subject} from "rxjs";
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'ui-stepper',
@@ -13,31 +15,41 @@ export class StepperComponent implements OnInit, AfterViewInit {
 
   grantStatusEnum = GrantStatusEnum
   grantStatus: string[] = []
-
   stepId: null | number = null
-
   formalStatuses = this.stepperService.getFormalStatuses()
+  setId$ = new Subject();
+  stepperInit$ = new Subject();
 
   _status = ''
   @Input() set status(data: string) {
-    if(data) {
+    if (data) {
       this._status = data
       this.stepId = this.stepperService.getActiveId(data)
-      console.log("this.stepId ", this.stepId, data )
-      if(this.stepper &&  this.stepId) {
-        this.stepper.selectedIndex =  this.stepId
+      if (this.stepId) {
+        this.setId$.next(this.stepId)
       }
     } else {
       this._status = 'no_status'
     }
+
   }
+
+  step$ = combineLatest([this.setId$, this.stepperInit$]).pipe(
+    tap(([id, init]) => {
+      if(id && typeof id === "number" && init && this.stepper) {
+        this.stepper.selectedIndex = id
+        this.cdr.markForCheck()
+      }
+    })
+  ).subscribe()
+
   get status(): string {
     return this._status
   }
 
   @ViewChild('stepper') stepper: MatStepper | undefined;
 
-  constructor(public stepperService: StepperService) {
+  constructor(private cdr: ChangeDetectorRef, public stepperService: StepperService) {
   }
 
   ngOnInit() {
@@ -46,6 +58,7 @@ export class StepperComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     if (this.stepper) {
       this.stepper._getIndicatorType = () => 'number'
+      this.stepperInit$.next(true)
     }
   }
 
