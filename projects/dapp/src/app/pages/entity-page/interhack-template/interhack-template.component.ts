e011f25cdf4949c6d3bd43ac372b0e6bfdbc2c19
mk <v.a.stekolschikov@gmail.com>
2021-01-12 15:22:48 +0200

    refactoring

--- projects/dapp/src/app/pages/entity-page/interhack-template/interhack-template.component.ts
+++ projects/dapp/src/app/pages/entity-page/interhack-template/interhack-template.component.ts
@@ -24,6 +24,7 @@ import {UserService} from '@services/user/user.service'
 import {AcceptWorkResultComponent} from '@ui/modals/accept-work-result/accept-work-result.component'
 import {combineLatest, Observable, Subject} from 'rxjs'
 import {SubmitSolutionComponent} from "@ui/modals/submit-solution/submit-solution.component";
+import {teamsAndSolutionsControls} from './functions'
 
 @Component({
   selector: 'app-interhack-template',
@@ -40,97 +41,9 @@ export class InterhackTemplateComponent implements TemplateComponentAbstract {
 
   grant$ = new Subject<ContractGrantModel>();
 
-  teamsAndSolutionsControls$: Observable<TeamsAndSolutionsControlsInterface> = combineLatest([this.userService.data, this.grant$])
-    .pipe(
-      tap(([user, grant]) => console.log(user, grant)),
-      // isShowSolutionControls
-      map(([user, grant]) => {
-        let result: TeamsAndSolutionsControlsInterface = {
-          isShowSolutionControls: true,
-          stepType: 'team',
-          isApplyBtn: false,
-          isSubmitSolutionBtn: false,
-          isShowAllTeam: false,
-          teamVoteKeys: [],
-          solutionVoteKeys: [],
-          leaderIds: [],
-          isShow: true
-        }
-        if(grant && grant.status && grant.status.value && grant.app) {
-          // isShowSolutionControls
-          const status = grant.status.value
-          if (status === this.grantStatusEnum.solutionChosen) {
-            result.isShowSolutionControls = false
-          }
-          // stepType
-          if ((status === this.grantStatusEnum.workStarted
-            || status === this.grantStatusEnum.proposed
-            || status === this.grantStatusEnum.readyToApply
-            || status === this.grantStatusEnum.workStarted)
-          ) {
-            result.stepType = 'team'
-          } else {
-            result.stepType = 'solution'
-          }
-          // isApplyBtn
-          if (user.roles.isAuth && status === this.grantStatusEnum.readyToApply) {
-            result.isApplyBtn = true
-          } else {
-            result.isApplyBtn = false
-          }
-          // isSubmitSolutionBtn
-          if (user.roles.isAuth && status === this.grantStatusEnum.workStarted) {
-            result.isSubmitSolutionBtn = true
-            grant.app.forEach((app) => {
-              if(app.solution && app.solution.key.includes(user.userAddress.slice(-15))) {
-                result.isSubmitSolutionBtn = false
-              }
-            })
-          } else {
-            result.isSubmitSolutionBtn = false
-          }
-          // isShowAllTeam
-          if(
-            status === this.grantStatusEnum.noStatus
-            || status === this.grantStatusEnum.proposed
-            || status === this.grantStatusEnum.readyToApply
-          ) {
-            result.isShowAllTeam = true
-          } else {
-            result.isShowAllTeam = false
-          }
-          // teamVoteKeys
-          let userKey = user.userAddress.slice(-15)
-          let teamKeys: string[] = []
-          if(grant && grant.app)
-            grant.app.forEach((e)=>{
-              if(e.key)
-                teamKeys.push(userKey + e.key)
-            })
-          teamKeys.forEach((key) => {
-            // @ts-ignore
-            if(grant.vh && grant.vh[key]) {
-              result.teamVoteKeys.push(key.slice(-25))
-            }
-          })
-          // solutionVoteKeys
-          grant.app.forEach((app) => {
-            if(app.voted && app.voted.solution)
-              result.solutionVoteKeys = app.voted.solution.value.split(';').filter(x => x) || []
-          })
-          userKey = user.userAddress
-          // leaderIds
-          grant.app.forEach((app) => {
-            result.leaderIds.push(app.leader.value)
-          })
-          // isShow
-          if(grant.status.value === this.grantStatusEnum.rejected && grant.app.length === 0) {
-            result.isShow = false
-          }
-        }
-        return result
-      })
-    )
+  teamsAndSolutionsControls$: Observable<TeamsAndSolutionsControlsInterface> = combineLatest(
+    [this.userService.data, this.grant$])
+    .pipe(map(([user, grant]) => teamsAndSolutionsControls(user, grant)))
 
   winnerIdentifier: string | null = null
   winnerIdentifier$ = this.grant$.pipe(
