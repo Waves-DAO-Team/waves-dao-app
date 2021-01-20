2b9a5592e1c4eb6c9fbd044be1a2f10ef2618dcd
mk <v.a.stekolschikov@gmail.com>
2021-01-20 12:33:08 +0200

    temp 2

--- projects/dapp/src/app/pages/members-page/members-page.component.ts
+++ projects/dapp/src/app/pages/members-page/members-page.component.ts
@@ -5,10 +5,15 @@ import {
   Inject,
   OnInit
 } from '@angular/core'
-import { Location } from '@angular/common'
-import { UserService } from '@services/user/user.service'
-import { API, APP_CONSTANTS, AppApiInterface, AppConstantsInterface } from '@constants'
-import { StaticService } from '@services/static/static.service'
+import {Location} from '@angular/common'
+import {UserService} from '@services/user/user.service'
+import {API, APP_CONSTANTS, AppApiInterface, AppConstantsInterface} from '@constants'
+import {StaticService} from '@services/static/static.service'
+import {DialogComponent} from "@ui/dialog/dialog.component";
+import {ApplyComponent} from "@ui/modals/apply/apply.component";
+import {SubmitCallBackApplyArg} from "@ui/dialog/dialog.tokens";
+import {take} from "rxjs/operators";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-members-page',
@@ -16,21 +21,44 @@ import { StaticService } from '@services/static/static.service'
   styleUrls: ['./members-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MembersPageComponent implements OnInit {
+export class MembersPageComponent {
   public readonly user$ = this.userService.data;
 
-  constructor (
-      private location: Location,
-      public userService: UserService,
-      public staticService: StaticService,
-      public cdr: ChangeDetectorRef,
-      @Inject(API) public readonly api: AppApiInterface,
-      @Inject(APP_CONSTANTS) public readonly constants: AppConstantsInterface
-  ) {}
+  constructor(
+    private dialog: MatDialog,
+    private location: Location,
+    public userService: UserService,
+    public staticService: StaticService,
+    public cdr: ChangeDetectorRef,
+    @Inject(API) public readonly api: AppApiInterface,
+    @Inject(APP_CONSTANTS) public readonly constants: AppConstantsInterface
+  ) {
+  }
 
-  goBack (): void {
+  goBack(): void {
     this.location.back()
   }
 
-  ngOnInit (): void {}
+  openAddDAOModal(): void {
+    // const dialog = this.dialog.open(DialogComponent, {
+    //   data: {
+    //     component: ApplyComponent,
+    //     params: {
+    //       grant: this.grant,
+    //       submitCallBack: (data: SubmitCallBackApplyArg) => {
+    //         this.disruptiveContractService.applyForTask(data.id, data.team, data.link)
+    //           .pipe(take(1))
+    //           .subscribe(() => {
+    //             dialog.close()
+    //             this.cdr.markForCheck()
+    //           })
+    //       }
+    //     }
+    //   }
+    // })
+  }
+
+  openAddWGModal() {
+    console.log('+++')
+  }
 }
