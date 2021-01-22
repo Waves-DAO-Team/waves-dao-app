9d739337f8837b1fc26f182c8849e98baac39c86
Grigory <aigre87@gmail.com>
2021-01-22 16:06:36 +0300

    vote-for-task fix, add mat-icon service

--- projects/ui/src/lib/vote-for-task/vote-for-task.component.ts
+++ projects/ui/src/lib/vote-for-task/vote-for-task.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core'
 
+type VoteStatus = 'like' | 'dislike';
+
 @Component({
   selector: 'ui-vote-for-task',
   templateUrl: './vote-for-task.component.html',
@@ -7,10 +9,16 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class VoteForTaskComponent {
+  voteStatus: VoteStatus | null = null;
+  buttonClicked = false;
   @Input() isShow = false
   @Input() isVote = false
 
-  @Output() newVoteEvent = new EventEmitter<'like' | 'dislike'>();
-
-  constructor () {}
+  @Output() newVoteEvent = new EventEmitter<VoteStatus>();
+  constructor () { }
+  vote (event: VoteStatus) {
+    this.voteStatus = event
+    this.newVoteEvent.emit(event)
+    this.buttonClicked = true
+  }
 }
