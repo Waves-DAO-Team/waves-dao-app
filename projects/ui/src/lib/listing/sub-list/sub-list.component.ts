5dcac57bb00f49735dd030454d09507af4f47208
Marina Lajus <elanor.me@gmail.com>
2021-02-01 10:33:32 +0200

    Style fix marina (#8)
    * Png icons replaced with svg
    * Installed protractor
    * Members title in en.json
    * Fix styles
    * Production 2242 voting grant (#7)
    * Fix code style warnings and added voting elements
    * Fix en.jso
    * Fix icon-logo and color on important cards fixed
    * Fix text bugs
    
    Co-authored-by: mlayus <mlayus@web3tech.ru>
    Co-authored-by: aigre87 <aigre87@gmail.com>

--- projects/ui/src/lib/listing/sub-list/sub-list.component.ts
+++ projects/ui/src/lib/listing/sub-list/sub-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Inject, Input, OnInit } from '@angular/core'
+import {Component, HostBinding, Inject, Input, OnInit, Type} from '@angular/core'
 import {
   ContractGrantExtendedModel
 } from '@services/contract/contract.model'
@@ -14,10 +14,13 @@ export class SubListComponent implements OnInit {
   @Input() contract: GrantsVariationType | null = null;
   @HostBinding('class.enable') @Input() grants: ContractGrantExtendedModel[] | null = null
   @Input() isImportant = false
+  // @ts-ignore
+  @Input() public type: 'default' | 'active'
   @Input() title: string | null = null
 
   constructor (
       @Inject(APP_CONSTANTS) public readonly constants: AppConstantsInterface
+
   ) {
   }
 
