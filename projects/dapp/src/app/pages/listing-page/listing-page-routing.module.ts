6349c2c858a850726c3008dcdf02f55a03592a73
mk <v.a.stekolschikov@gmail.com>
2021-02-01 18:52:31 +0200

    fix

--- projects/dapp/src/app/pages/listing-page/listing-page-routing.module.ts
+++ projects/dapp/src/app/pages/listing-page/listing-page-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { ListingPageComponent } from './listing-page.component'
+import {DialogModule} from "@ui/dialog/dialog.module";
 
 const routes: Routes = [
   {
@@ -11,7 +12,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), DialogModule],
   exports: [RouterModule]
 })
 export class ListingPageRoutingModule { }
