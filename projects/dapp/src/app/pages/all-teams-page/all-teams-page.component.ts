28f4d5b7b2e324228b34a1c9a5193312941ddf14
mk <v.a.stekolschikov@gmail.com>
2021-02-02 18:33:09 +0200

    добавил  grantUrl$

--- projects/dapp/src/app/pages/all-teams-page/all-teams-page.component.ts
+++ projects/dapp/src/app/pages/all-teams-page/all-teams-page.component.ts
@@ -2,9 +2,10 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {Location} from "@angular/common";
 import {LoadingWrapperModel} from "@libs/loading-wrapper/loading-wrapper";
 import {ContractGrantAppModel, ContractGrantModel, TeamsScoreLinkModel} from "@services/contract/contract.model";
-import {filter, map, tap} from "rxjs/operators";
+import {filter, map, switchMap, tap} from "rxjs/operators";
 import {Observable} from "rxjs";
 import {ALL_TEAM, ALL_TEAM_PAGE_PROVIDERS} from "./all-teams-page-routing.providers";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-all-teams-page',
@@ -14,37 +15,41 @@ import {ALL_TEAM, ALL_TEAM_PAGE_PROVIDERS} from "./all-teams-page-routing.provid
 })
 export class AllTeamsPageComponent {
 
+  public grantUrl$ = this.route.paramMap
+    .pipe(
+      // @ts-ignore
+      map((e) => e.params)
+    )
+
   public title$: Observable<String> = this.entity.data$
     .pipe(
-      tap((data) => console.log('---1-0', data)),
       filter((data) => data.title != undefined && data.title.value != undefined),
       // @ts-ignore
       map((data) => data.title.value),
-      tap((data) => console.log('---1-1', data))
     )
 
-  // score name about
-  // public teams$: Observable<TeamsScoreLinkModel[]> = this.entity.data$
-  //   .pipe(
-  //     filter((data) => data != undefined && data.app != undefined),
-  //     map((data) => data.app),
-  //     map((data) => {
-  //       let res: TeamsScoreLinkModel[] = []
-  //       if(data) {
-  //         data.forEach((d) => {
-  //           res.push({
-  //             name: d.name.value,
-  //             link: d.link.value,
-  //             score: d.score != undefined ? d.score.value : 0
-  //           })
-  //         })
-  //       }
-  //       return res
-  //     }),
-  //     tap((data) => console.log('---2', data))
-  //   )
+  public teams$: Observable<TeamsScoreLinkModel[]> = this.entity.data$
+    .pipe(
+      filter((data) => data != undefined && data.app != undefined),
+      map((data) => data.app),
+      map((data) => {
+        let res: TeamsScoreLinkModel[] = []
+        if (data) {
+          data.forEach((d) => {
+            res.push({
+              name: d.name.value,
+              link: d.link.value,
+              score: d.score && d.score.applicant && d.score.applicant.value ? d.score.applicant.value : 0
+            })
+          })
+        }
+        return res
+      }),
+      tap((data) => console.log('---2', data))
+    )
 
   constructor(
+    private route: ActivatedRoute, // eslint-disable-line
     private readonly location: Location, // eslint-disable-line
     @Inject(ALL_TEAM) public entity: LoadingWrapperModel<ContractGrantModel>, // eslint-disable-line
   ) {
