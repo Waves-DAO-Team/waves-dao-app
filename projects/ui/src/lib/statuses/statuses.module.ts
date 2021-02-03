798d06a855f934f32e6fd537bc8444d6e8c68a92
mk <v.a.stekolschikov@gmail.com>
2021-02-03 11:28:00 +0200

    добавил языковые константы has_solution и rejected

--- projects/ui/src/lib/statuses/statuses.module.ts
+++ projects/ui/src/lib/statuses/statuses.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatusesComponent } from './statuses.component';
+import {TranslocoModule} from "@ngneat/transloco";
 
 
 
 @NgModule({
   declarations: [StatusesComponent],
-  imports: [
-    CommonModule
-  ],
+    imports: [
+        CommonModule,
+        TranslocoModule
+    ],
   exports: [StatusesComponent]
 })
 export class StatusesModule { }
