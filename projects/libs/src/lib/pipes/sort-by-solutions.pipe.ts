06c5431109899828881f7a72e2582944973f5c47
mk <v.a.stekolschikov@gmail.com>
2021-02-10 11:50:49 +0200

    In the interhack, the kemand who was set minus one during the selection did not go to the sub-list. Only those who passed the selection with a positive result should remain on the list.
    After voting, in the box it is better to make a layout with a large number that all other users see.
    
    According to the layouts, we have the same score numbers. Now they all jump from interface to interface

--- projects/libs/src/lib/pipes/sort-by-solutions.pipe.ts
+++ projects/libs/src/lib/pipes/sort-by-solutions.pipe.ts
@@ -9,9 +9,27 @@ import { ContractGrantAppModel } from '@services/contract/contract.model'
 @Pipe({ name: 'sortBySolutions' })
 export class SortBySolutionsPipe implements PipeTransform {
   transform (value: ContractGrantAppModel[], order: string = 'asc'): ContractGrantAppModel[] {
-    let sorted = _.sortBy(value, (e) => {
-      return e?.score?.solution?.value;
-    }).reverse();
-    return sorted
+    console.log('this.detectSortType(value)', this.detectSortType(value), value)
+    if (this.detectSortType(value) === 'team') {
+      // value = _.orderBy(value, ['score', 'value'], [order === 'desc' ? order : 'asc'])
+      // value = _.orderBy(value, ['process', 'value'], [order === 'desc' ? order : 'asc'])
+      value = _.sortBy(value, (e) => {
+        return e?.score?.applicant?.value;
+      }).reverse();
+    } else {
+      value = _.sortBy(value, (e) => {
+        return e?.score?.solution?.value;
+      }).reverse();
+    }
+    return value
+  }
+  private detectSortType(value: ContractGrantAppModel[]): 'team' | 'solution' {
+    let isTeam = true
+    value.forEach((e)=>{
+      if (e.score?.solution?.value) {
+        isTeam = false
+      }
+    })
+    return isTeam ? 'team' : 'solution'
   }
 }
