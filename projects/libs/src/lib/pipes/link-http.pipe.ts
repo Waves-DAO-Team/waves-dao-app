c00f6224064e937c5bbed2a9a11c9f83e52ec98f
Albert Iblyaminov <albert@iblyaminov.io>
2021-03-15 05:50:56 +0300

    Update README.md and lint

--- projects/libs/src/lib/pipes/link-http.pipe.ts
+++ projects/libs/src/lib/pipes/link-http.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core'
 export class LinkHttpPipe implements PipeTransform {
   transform (href: string | null): string {
     if (!href) {
-      return '';
+      return ''
     }
 
     if(typeof href  !== 'string') {
-      return href;
+      return href
     }
     return href.startsWith('http://') || href.startsWith('https://') ? href : 'http://' + href
   }
