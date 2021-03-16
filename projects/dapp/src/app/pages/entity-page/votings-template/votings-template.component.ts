57784082e08848312c025ad459d6305039b70097
mk <v.a.stekolschikov@gmail.com>
2021-03-16 12:25:49 +0200

    removed old methods from votings

--- projects/dapp/src/app/pages/entity-page/votings-template/votings-template.component.ts
+++ projects/dapp/src/app/pages/entity-page/votings-template/votings-template.component.ts
@@ -4,7 +4,7 @@ import {GrantStatusEnum, GrantsVariationType} from '@services/static/static.mode
 import {DisruptiveContractService} from '@services/contract/disruptive-contract.service'
 import {MatSnackBar} from '@angular/material/snack-bar'
 import {SignerService} from '@services/signer/signer.service'
-import {filter, map, take, takeUntil} from 'rxjs/operators'
+import {filter, map, publishReplay, refCount, take, takeUntil, tap} from 'rxjs/operators'
 import {translate} from '@ngneat/transloco'
 import {DialogComponent} from '@ui/dialog/dialog.component'
 import {ApplyComponent} from '@ui/modals/apply/apply.component'
@@ -37,49 +37,42 @@ import {
 import {ActivatedRoute} from '@angular/router'
 import {IScore} from '@services/interface'
 import {FinishApplicantsVotingComponent} from '@ui/modals/finish-applicants-voting/finish-applicants-voting.component'
+import {Web3TemplateInterface} from "@pages/entity-page/web3-template/web3-template.interface";
+import {log} from "@libs/log";
+import {Async} from "@libs/decorators";
 
 @Component({
   selector: 'app-votings-template',
   templateUrl: './votings-template.component.html',
   styleUrls: ['./votings-template.component.scss']
 })
-export class VotingsTemplateComponent implements TemplateComponentAbstract, OnDestroy {
+export class VotingsTemplateComponent {
+
   @Input() public readonly contract!: GrantsVariationType
-
-  grantStatusEnum = GrantStatusEnum
-
-  grant$ = new BehaviorSubject<ContractGrantModel>({})
+  @Async() @Input('grant') public readonly grant$!: Observable<ContractGrantModel>
 
   private readonly destroyed$ = new Subject()
 
-  private user$ = this.userService.data
-    .pipe(
-      takeUntil(this.destroyed$),
-      filter(() => this.inputGrant?.id !== undefined)
-    )
-    .subscribe(() => this.prepareVoteForTaskData(this.inputGrant))
-  
-
-  public voteForTaskData = {
-    isShow: false,
-    isVote: false,
-    isVoteInProcess: false
-  }
-
-  @Input() set grant(data: ContractGrantModel) {
-    if (data !== this.inputGrant) {
-      this.inputGrant = data
-      this.prepareVoteForTaskData(data)
-    }
-    this.grant$.next(data)
-  }
-
-  get grant():
-    ContractGrantModel {
-    return this.inputGrant
-  }
-
-  private inputGrant: ContractGrantModel = {}
+  public entityData$: Observable<Web3TemplateInterface> = combineLatest([this.userService.stream$, this.grant$]).pipe(
+    takeUntil(this.destroyed$),
+    map(([user, grant]) => ({
+      ...grant,
+      isApproved: grant?.status?.value === GrantStatusEnum.approved,
+      isLeader: grant?.leader?.value === user.userAddress,
+      isAmount: !!grant?.voting?.amount,
+      isVotingStarted: grant?.status?.value === GrantStatusEnum.votingStarted,
+      isWG: user.roles.isWG,
+      isReward: !!grant?.reward?.value,
+      isNewGrant: !grant?.status?.value,
+      isCanceled: grant?.status?.value !== GrantStatusEnum.workFinished && grant?.status?.value !== GrantStatusEnum.rejected,
+      isWorkStarted: grant?.status?.value === GrantStatusEnum.workStarted,
+      isShowVoting: user.roles.isDAO && grant?.status?.value === GrantStatusEnum.votingStarted,
+      isVoteForGrant: user.roles.isDAO && !!grant?.voted && !!grant?.voted[user.userAddress]
+    })),
+    log('VotingsTemplateComponent::entityData$'),
+    publishReplay(1),
+    refCount()
+  )
 
   constructor(
     private route: ActivatedRoute, // eslint-disable-line
@@ -92,17 +85,6 @@ export class VotingsTemplateComponent implements TemplateComponentAbstract, OnDe
   ) {
   }
 
-  vote(value: 'like' | 'dislike'): void {
-    const id = this.grant.id || ''
-    this.voteForTaskData.isVoteInProcess = true
-    this.disruptiveContractService.voteForTaskProposal(id, value).subscribe({
-      complete: () => {
-        this.voteForTaskData.isVoteInProcess = false
-        this.cdr.markForCheck()
-      }
-    })
-  }
-
   signup(): void {
     this.signerService.login()
       .pipe(take(1))
@@ -112,137 +94,4 @@ export class VotingsTemplateComponent implements TemplateComponentAbstract, OnDe
       })
   }
 
-  openApplyModal(): void {
-    const dialog = this.dialog.open(DialogComponent, {
-      width: '500px',
-      maxWidth: '100vw',
-      data: {
-        component: ApplyComponent,
-        params: {
-          grant: this.grant,
-          submitCallBack: (data: SubmitCallBackApplyArg) => {
-            this.disruptiveContractService.applyForTask(data.id, data.team, data.link)
-              .pipe(take(1))
-              .subscribe()
-            dialog.close()
-            this.cdr.markForCheck()
-          }
-        }
-      }
-    })
-  }
-
-  voteTeam($event: VoteTeamEventInterface): void {
-    const id = this.grant?.id as string
-    const teamId = $event.teamIdentifier
-    const vote = $event.voteValue
-    this.disruptiveContractService.voteForApplicant(id, teamId, vote).subscribe()
-  }
-
-  finishVote(): void {
-    this.disruptiveContractService.finishTaskProposalVoting(this.grant?.id as string).subscribe()
-  }
-
-  startWork(): void {
-    this.disruptiveContractService.startWork(this.grant?.id as string).subscribe()
-  }
-
-  reject(): void {
-    this.disruptiveContractService.rejectTask(this.grant?.id as string).subscribe()
-  }
-
-  acceptWorkResult(): void {
-    const dialog = this.dialog.open(DialogComponent, {
-      width: '500px',
-      maxWidth: '100vw',
-      data: {
-        component: AcceptWorkResultComponent,
-        params: {
-          title: translate('modal.texts.accept_work_result'),
-          submitBtnText: translate('modal.btn.apply'),
-          submitCallBack: (data: SubmitCallBackAcceptWorkResultArg) => {
-            this.disruptiveContractService.acceptWorkResult(this.grant?.id as string, data.reportLink)
-              .subscribe()
-            dialog.close()
-            this.cdr.markForCheck()
-          }
-        }
-      }
-    })
-  }
-
-  finishApplicantsVote(): void {
-
-    const teamIdList: string[] = []
-    this.grant.app?.forEach(el => {
-      if (el?.score?.value && +el?.score?.value > 0) {
-        teamIdList.push(el.id.value)
-      }
-    })
-
-    const dialog = this.dialog.open(DialogComponent, {
-      width: '500px',
-      maxWidth: '100vw',
-      data: {
-        component: FinishApplicantsVotingComponent,
-        params: {
-          title: translate('entity.finish_applicants_voting'),
-          submitBtnText: translate('modal.btn.propose_grant'),
-          grantId: this.grant?.id,
-          teamIdList,
-          proposedWinner: getWinnerTeamId(this.grant),
-          submitCallBack: (data: FinishApplicantsVotingArg) => {
-            const id = this.grant.id as string
-            const winnerTeamId = data.winnerTeamId
-            if (id && winnerTeamId) {
-              this.disruptiveContractService.finishApplicantsVoting(id, winnerTeamId).subscribe()
-            }
-            dialog.close()
-            this.cdr.markForCheck()
-          }
-        }
-      }
-    })
-  }
-
-  addReward(): void {
-    const dialog = this.dialog.open(DialogComponent, {
-      width: '500px',
-      maxWidth: '100vw',
-      data: {
-        component: AddRewardComponent,
-        params: {
-          title: !this.grant?.status?.value ? translate('entity.add_reward') : translate('entity.edit_task_details'),
-          submitBtnText: translate('modal.btn.propose_grant'),
-          grantId: this.grant?.id,
-          submitCallBack: (data: SubmitCallBackRewardArg) => {
-            const id = this.grant?.id
-            const reward = parseInt(data.reward, 10).toString()
-            if (id) {
-              this.disruptiveContractService.addReward(id, reward).subscribe()
-            }
-            dialog.close()
-            this.cdr.markForCheck()
-          }
-        }
-      }
-    })
-  }
-
-  private prepareVoteForTaskData(grant: ContractGrantModel) {
-    if (this.userService.data.getValue().roles.isDAO && grant.status?.value === this.grantStatusEnum.proposed) {
-      this.voteForTaskData.isShow = true
-    } else {
-      this.voteForTaskData.isShow = false
-    }
-    if (grant && grant.id && grant?.voted && !!grant?.voted[this.userService.data.getValue()?.userAddress]) {
-      this.voteForTaskData.isVote = true
-    } else {
-      this.voteForTaskData.isVote = false
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.destroyed$.next()
-  }
 }
