1f09c9d33418698b6f90a7c9048ca6d7b462f127
mk <v.a.stekolschikov@gmail.com>
2021-04-01 15:49:09 +0300

    marge

---
+++ projects/ui/src/lib/hash/hash.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core'
+
+@Component({
+  selector: 'ui-hash',
+  templateUrl: './hash.component.html',
+  styleUrls: ['./hash.component.scss']
+})
+export class HashComponent implements OnInit {
+
+  @Input() public readonly status: boolean | null = null
+  @Input() public readonly linkText: string | null = null
+
+  constructor () { }
+
+  ngOnInit (): void {}
+
+}
